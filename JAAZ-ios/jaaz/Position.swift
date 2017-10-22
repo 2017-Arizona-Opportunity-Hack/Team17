951dc163a8b9f7a805a23075a0271ec91e710d95
Aritro Majumdar <Aritro@Anikets-MacBook-Pro-2.local>
2017-10-22 13:23:27 -0700

    Aritro's Android changes

---
+++ JAAZ-ios/jaaz/Position.swift
@@ -0,0 +1,13 @@
+//
+//  Position.swift
+//  jaaz
+//
+//  Created by Aritro Majumdar on 10/22/17.
+//  Copyright © 2017 Team 17. All rights reserved.
+//
+
+import UIKit
+
+class Position: FIRFirestore {
+
+}
