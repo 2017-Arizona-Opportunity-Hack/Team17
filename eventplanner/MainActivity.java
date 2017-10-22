6fac5d03f8d801ea33827f02ae49ba0a4795630f
Ronnie Vu <ronnievu@asu.edu>
2017-10-22 12:58:04 -0700

    Added UI files

---
+++ eventplanner/MainActivity.java
@@ -0,0 +1,67 @@
+package com.juniorachievementofarizona.eventplanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements myInterface {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+
+
+        String[] myDataset = {"91st Palms Christian\n2020 E. Baseline Rd. Phoenix\n1:00PM - 3:00PM\n\nPrivate"};
+        mAdapter = new MyAdapter(myDataset, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+
+    }
+
+
+    public void changeToLanding(String data){
+        Intent nextPage = new Intent(MainActivity.this, Landing_Page.class);
+        nextPage.putExtra("Data", data);
+        startActivity(nextPage);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
---
+++ eventplanner/myInterface.java
@@ -0,0 +1,9 @@
+package com.juniorachievementofarizona.eventplanner;
+
+/**
+ * Created by ronnievu on 10/22/17.
+ */
+
+public interface myInterface {
+    void changeToLanding(String s);
+}
